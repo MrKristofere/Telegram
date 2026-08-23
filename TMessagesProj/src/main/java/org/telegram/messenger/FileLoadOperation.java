bf2e6bc2ec769c427f49ddb8ad61893ccb888f64
MrKristofere <119357545+MrKristofere@users.noreply.github.com>
2026-08-23 05:48:32 +0300

    [Test]Boost file download speed (large chunks)
    Adjust download parameters based on preload settings.

--- TMessagesProj/src/main/java/org/telegram/messenger/FileLoadOperation.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/FileLoadOperation.java
@@ -287,13 +287,13 @@ public class FileLoadOperation {
 
     private void updateParams() {
         if ((preloadPrefixSize > 0 || MessagesController.getInstance(currentAccount).getfileExperimentalParams) && !forceSmallChunk) {
+            downloadChunkSizeBig = 1024 * 1024;
+            maxDownloadRequests = 12;
+            maxDownloadRequestsBig = 12;
+        } else {
             downloadChunkSizeBig = 1024 * 512;
             maxDownloadRequests = 8;
             maxDownloadRequestsBig = 8;
-        } else {
-            downloadChunkSizeBig = 1024 * 128;
-            maxDownloadRequests = 4;
-            maxDownloadRequestsBig = 4;
         }
         maxCdnParts = (int) (FileLoader.DEFAULT_MAX_FILE_SIZE / downloadChunkSizeBig);
     }

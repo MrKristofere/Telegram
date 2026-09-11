72ee1cfaf26bc065d4a0dc258fc01e6665aea5b5
MrKristofere <119357545+MrKristofere@users.noreply.github.com>
2026-09-11 03:00:14 +0300

    Allow screenshots in a "view-once voice"

--- TMessagesProj/src/main/java/org/telegram/ui/SecretVoicePlayer.java
+++ TMessagesProj/src/main/java/org/telegram/ui/SecretVoicePlayer.java
@@ -239,7 +239,7 @@ public class SecretVoicePlayer extends Dialog {
             WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS |
             WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION;
         if (!BuildVars.DEBUG_PRIVATE_VERSION) {
-            params.flags |= WindowManager.LayoutParams.FLAG_SECURE;
+            //params.flags |= WindowManager.LayoutParams.FLAG_SECURE;
             AndroidUtilities.logFlagSecure();
         }
         params.flags |= WindowManager.LayoutParams.FLAG_FULLSCREEN;
